2d0a4dde58d5f5642e93cd51ecd14548a7980007
Piotr Markowski <piotrjakubmarkowski@gmail.com>
2018-01-19 19:04:45 +0100

    stworzenie projektu spring boot

---
+++ src/main/java/pl/lodz/p/edu/Controller.java
@@ -0,0 +1,14 @@
+package pl.lodz.p.edu;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
