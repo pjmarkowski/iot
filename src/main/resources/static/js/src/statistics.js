4e8ccd0c0f5d5d1943516764d59e2d6f18458f1c
Piotr Markowski <piotrjakubmarkowski@gmail.com>
2018-09-10 18:41:48 +0200

    historia roweru ograniczona datami

--- src/main/resources/static/js/src/statistics.js
+++ src/main/resources/static/js/src/statistics.js
@@ -10,7 +10,8 @@ function initialize() {
         mapTypeId: 'roadmap'
     };
     var bikeList = [];
-
+    var from="";
+    var to="";
     $.getJSON("http://localhost:8080/getAllBikes", function(json){
         bikeList = json;
         var str ="";
@@ -48,28 +49,40 @@ function initialize() {
                         for( i = 0; i < data.length; i++ ) {
                             for(j = 0; j < jsonhistory.length; j++) {
                                 if(data[i].stationNumber == jsonhistory[j].stationNumber) {
-                                    console.log(JSON.stringify(data[i].latitude));
-                                    var lat=data[i].latitude;
-                                    var long= data[i].longtitude;
-                                    var position = new google.maps.LatLng(lat, long);
-                                    bounds.extend(position);
-                                    linePlanCords.push(position);
-                                    marker = new google.maps.Marker({
-                                        position: position,
-                                        map: map,
-                                        title: data[i].stationName +" "+jsonhistory[j].date
-                                    });
 
-                                    google.maps.event.addListener(marker, 'click', (function(marker, i, j) {
-                                        return function() {
-                                            infoWindow.setContent('<div class="info_content">' +
-                                                '<h3>'+data[i].stationName+'</h3>' +
+                                    var d1 = from.split("-");
+                                    var d2 = to.split("-");
+                                    var cc = jsonhistory[j].date.substring(0, 10).split("-");
 
-                                                '</div>');
-                                            infoWindow.open(map, marker);
-                                        }
-                                    })(marker, i));
-                                    map.fitBounds(bounds);
+                                    var f = new Date(d1[0], parseInt(d1[1]) - 1, d1[2]);
+                                    var t = new Date(d2[0], parseInt(d2[1]) - 1, d2[2]);
+                                    var c = new Date(cc[0], parseInt(cc[1]) - 1, cc[2]);
+
+                                    if(c > f && c < t) {
+
+                                        console.log(JSON.stringify(data[i].latitude));
+                                        var lat = data[i].latitude;
+                                        var long = data[i].longtitude;
+                                        var position = new google.maps.LatLng(lat, long);
+                                        bounds.extend(position);
+                                        linePlanCords.push(position);
+                                        marker = new google.maps.Marker({
+                                            position: position,
+                                            map: map,
+                                            title: data[i].stationName + " " + jsonhistory[j].date
+                                        });
+
+                                        google.maps.event.addListener(marker, 'click', (function (marker, i, j) {
+                                            return function () {
+                                                infoWindow.setContent('<div class="info_content">' +
+                                                    '<h3>' + data[i].stationName + '</h3>' +
+
+                                                    '</div>');
+                                                infoWindow.open(map, marker);
+                                            }
+                                        })(marker, i));
+                                        map.fitBounds(bounds);
+                                    };
                                 };
 
                             }
@@ -95,6 +108,16 @@ function initialize() {
                     });
                 });
             });
+        });
+    });
+
+    $(function() {
+        $('input[name="daterange"]').daterangepicker({
+            opens: 'left'
+        }, function(start, end, label) {
+            console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+            from = start.format('YYYY-MM-DD');
+            to = end.format('YYYY-MM-DD');
         });
     });
 };
